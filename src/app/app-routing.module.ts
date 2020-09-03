9bb7df00b3193138c762321bda0f1373c29423b3
np1goal <np1goal@gmail.com>
2020-09-02 20:29:34 -0700

    Home page animation
    Animations added to home page text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/home/home.component';
-import { CssAnimationTutorialComponent } from 'src/app/css-animation-tutorial/css-animation-tutorial.component';
-import { ScssAnimationTutorialComponent } from 'src/app/scss-animation-tutorial/scss-animation-tutorial.component';
 import { SvgAnimationTutorialComponent } from 'src/app/svg-animation-tutorial/svg-animation-tutorial.component';
-import { JsAnimationTutorialComponent } from 'src/app/js-animation-tutorial/js-animation-tutorial.component';
 import { LoadingAnimationsComponent } from './loading-animations/loading-animations.component';
 import { ButtonAnimationsComponent } from './button-animations/button-animations.component';
 import { BackgroundAnimationsComponent } from './background-animations/background-animations.component';
@@ -13,10 +10,7 @@ import { BorderAnimationsComponent } from './border-animations/border-animations
 
 const routes: Routes = [
   {path:"", component: HomeComponent},
-  {path:"css-animation-tutorial", component: CssAnimationTutorialComponent},
-  {path:"scss-animation-tutorial", component: ScssAnimationTutorialComponent},
-  {path:"svg-animation-tutorial", component: SvgAnimationTutorialComponent},
-  {path:"js-animation-tutorial", component: JsAnimationTutorialComponent},
+  {path:"svg-animations", component: SvgAnimationTutorialComponent},
   {path:"loading-animations", component: LoadingAnimationsComponent},
   {path:"button-animations", component: ButtonAnimationsComponent},
   {path:"background-animations", component: BackgroundAnimationsComponent},
@@ -29,8 +23,6 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [  HomeComponent, CssAnimationTutorialComponent,
-                                    ScssAnimationTutorialComponent, SvgAnimationTutorialComponent,
-                                    JsAnimationTutorialComponent, LoadingAnimationsComponent,
-                                    ButtonAnimationsComponent, BackgroundAnimationsComponent, 
-                                    BorderAnimationsComponent]
+export const routingComponents = [  HomeComponent, SvgAnimationTutorialComponent,
+                                    LoadingAnimationsComponent, ButtonAnimationsComponent, 
+                                    BackgroundAnimationsComponent, BorderAnimationsComponent]
